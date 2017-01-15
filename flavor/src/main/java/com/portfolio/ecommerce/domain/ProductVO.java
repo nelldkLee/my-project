884a788e519bf3b2aac953906bcc2d316fde548a
LAPTOP-05IRIC15\nelld <dntdj0>
2017-01-15 22:47:35 +0900

    쇼핑몰 삼품 도메인

---
+++ flavor/src/main/java/com/portfolio/ecommerce/domain/ProductVO.java
@@ -0,0 +1,12 @@
+package com.portfolio.ecommerce.domain;
+
+import java.util.Date;
+
+public class ProductVO {
+
+	private String productName;
+	private int productPrice;
+	private Date createdDate;
+	private Date updatedDate;
+	
+}
